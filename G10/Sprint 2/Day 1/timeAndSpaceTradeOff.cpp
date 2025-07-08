eba38eb57ac3486b20632c6c356f6f3c321b5032
aanandsinghyadav <anandsinghyadav9092@gmail.com>
2025-07-08 08:42:33 +0530

    Done

---
+++ G10/Sprint 2/Day 1/timeAndSpaceTradeOff.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/*
+Time-Space TradeOff - You try to balance both time and space while designing the optimised algorithm.
+
+Problem: You need to find the number effienciently.
+let number is 6
+
+Option 1:
+size of below array(n): 7 
+int [] arr = [1,2,3,4,5,6,7];
+There is no extra space, we have used there for taking time.
+
+for loop: visiting the each and every element: worst case: n time loop will run.
+
+Option 2:
+int [] arr = [1,2,3,4,5,6,7];
+map<int, int> numbers;
+Loop: stores give array numbers in form of key-value pair.
+{1: 1, 2: 1, 3: 3, 4:4, 5:5, 6:6, 7:7};
+
+Let access 1: numbers[1]; //Constant Time
+
+
+
+
+*/
+
+}
